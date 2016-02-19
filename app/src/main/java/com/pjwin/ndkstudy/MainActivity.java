809d928ff98887fcb4fe28b523e01a2e156bcf24
pjwin <pjwingold@hotmail.com>
2016-02-20 01:03:22 +1100

    added codes to handle image process off the main thread

--- app/src/main/java/com/pjwin/ndkstudy/MainActivity.java
+++ app/src/main/java/com/pjwin/ndkstudy/MainActivity.java
@@ -1,7 +1,15 @@
 package com.pjwin.ndkstudy;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -9,12 +17,18 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.pjwin.ndkstudy.entity.Customer;
+import com.pjwin.ndkstudy.task.ApplyEffectService;
+import com.pjwin.ndkstudy.task.ApplyEffectThread;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.Locale;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, ApplyEffectThread.ProcessImageCallback {
     private static final String TAG = "MainActivity";
     private int count = 0;
     private String labelStr = "";
@@ -22,14 +36,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     static {
         System.loadLibrary("ndk_study");
-        //System.loadLibrary("ndk_test");
     }
 
     private ImageView imageView;
-    private Bitmap mainImage;
+    private static Bitmap mainImage;
     private Button resetBtn, javaUpdateBtn, jniUpdateBtn;
     private DisplayMetrics dm;
     private TextView titleText;
+    private ProgressBar pb;
+
+    private ExecutorService mService;
+    private ApplyEffectService applyEffectService;
+    private boolean mBound;
+    private long start;
+    private long after;
+
+    public static final int PROCESS_JAVA = 1;
+    public static final int PROCESS_JNI = 2;
+
+    private int processId;
+    private Intent serviceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +65,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         titleText = (TextView) findViewById(R.id.titleText);
         imageView = (ImageView) findViewById(R.id.image);
+        pb = (ProgressBar) findViewById(R.id.progressBar);
         resetBtn = (Button) findViewById(R.id.reset);
         javaUpdateBtn = (Button) findViewById(R.id.javaUpdate);
         jniUpdateBtn = (Button) findViewById(R.id.jniUpdate);
@@ -57,11 +84,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Log.i(TAG, String.valueOf(count));
         Log.i(TAG, labelStr);
 
-        //mainImage = ((BitmapDrawable) (imageView.getDrawable())).getBitmap();
         dm = new DisplayMetrics();
         WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
         wm.getDefaultDisplay().getMetrics(dm);
         loadImage();
+
+        serviceIntent = new Intent(this, ApplyEffectService.class);
+        bindService(serviceIntent, mConnection, BIND_AUTO_CREATE);
+        mService = Executors.newSingleThreadExecutor();
+
+        //finish();
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
     }
 
     private void loadImage() {
@@ -83,7 +121,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        Log.i(TAG, "onclick");
+        Log.i(TAG, Thread.currentThread().getId() + " id");
         int vId = v.getId();
         switch (vId) {
             case R.id.javaUpdate:
@@ -100,31 +138,127 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    private void javaUpdate() {
-        long start = System.currentTimeMillis();
-        Bitmap jBitmap = ImageUtil.toImageRelief(mainImage);
-        imageView.setImageBitmap(jBitmap);
-        long after = System.currentTimeMillis();
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+        IntentFilter filter = new IntentFilter(ApplyEffectService.ACTION);
+        LocalBroadcastManager.getInstance(this).registerReceiver(mReceiver, filter);
+    }
 
-        Log.i(TAG, " " + (after - start) + " Java");
-        titleText.setText(String.format("Time taken to completed process in Java: (%f) seconds", (float) (after - start) / 1000));
-        Toast.makeText(this, "Process finished", Toast.LENGTH_SHORT).show();
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mReceiver);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int resultCode = intent.getIntExtra(ApplyEffectService.RESULT_CODE, ApplyEffectService.SUCCESS);
+            //if (resultCode == ApplyEffectService.SUCCESS) {
+                onImageProcessFinished(applyEffectService.getResultBitmap());
+            //}
+            //unbindService(mConnection);
+        }
+    };
+
+    private final ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            ApplyEffectService.ApplyEffectServiceBinder mBinder = (ApplyEffectService.ApplyEffectServiceBinder) service;
+            applyEffectService = mBinder.getService();
+            applyEffectService.setSourceBitmap(mainImage);
+            mBound = true;
+            Log.i(TAG, "onServiceConnected");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBound = false;
+            Log.i(TAG, "onServiceDisconnected");
+        }
+    };
+
+    private void javaUpdate() {
+        //finish();
+        start = System.currentTimeMillis();
+        processId = PROCESS_JAVA;
+        pb.setVisibility(View.VISIBLE);
+
+        processWithService();
+        //processWithThread();
     }
 
     private void jniUpdate() {
-        long start = System.currentTimeMillis();
+        start = System.currentTimeMillis();
+        processId = PROCESS_JNI;
+        pb.setVisibility(View.VISIBLE);
 
-        Bitmap jBitmap = ImageUtil.toImageReliefJni(mainImage);
+        processWithService();
+        //processWithThread
+    }
 
-        imageView.setImageBitmap(jBitmap);
-        long after = System.currentTimeMillis();
+    private void processWithThread() {
+        mService.execute(new ApplyEffectThread(mainImage, processId, this));
+    }
 
-        titleText.setText(String.format("Time taken to completed process with JNI: (%f) seconds", (float) (after - start) / 1000));
-        Log.i(TAG, " " + (after - start) + " JNI");
+    private void processWithIntentService() {
+
+    }
+
+    private void processWithService() {
+        //serviceIntent.putExtra(ApplyEffectService.SERVICE_METHOD, processId);
+        //startService(serviceIntent);
+        if (mBound) {
+            applyEffectService.processImage(processId);
+        }
     }
 
     private void resetImage() {
+        processId = 0;
         imageView.setImageBitmap(mainImage);
     }
 
+    @Override
+    protected void onDestroy() {
+        //mService.shutdown();
+        if (mBound) {
+            unbindService(mConnection);
+            mBound = false;
+        }
+        Log.i(TAG, "onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onProcessedFinished(Bitmap bitmap) {
+        onImageProcessFinished(bitmap);
+    }
+
+    private void onImageProcessFinished(Bitmap bitmap) {
+        imageView.setImageBitmap(bitmap);
+        after = System.currentTimeMillis();
+        pb.setVisibility(View.GONE);
+
+        switch (processId) {
+            case PROCESS_JAVA:
+                Log.i(TAG, " " + (after - start) + " Java");
+                titleText.setText(String.format(Locale.getDefault(), "Time taken to completed process in Java: (%f) seconds", (float) (after - start) / 1000));
+                break;
+            case PROCESS_JNI :
+                Log.i(TAG, " " + (after - start) + " JNI");
+                titleText.setText(String.format(Locale.getDefault(), "Time taken to completed process with JNI: (%f) seconds", (float) (after - start) / 1000));
+                break;
+            default:
+                break;
+        }
+    }
 }
